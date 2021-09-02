a99205af64f72e8517814f0777bce4635276c932
Scott Mathis <smathis@vmware.com>
2021-09-02 11:48:51 -0400

    Clarity Core components added

---
+++ NpmJS/src/index.js
@@ -0,0 +1,7 @@
+import '@cds/core/button/register.js';
+import '@cds/core/alert/register.js';
+import '@cds/core/icon/register.js';
+
+import { ClarityIcons, helpIcon, homeIcon, calculatorIcon, rackServerIcon, treeViewIcon } from '@cds/core/icon';
+
+ClarityIcons.addIcons(helpIcon, homeIcon, calculatorIcon, rackServerIcon, treeViewIcon);
